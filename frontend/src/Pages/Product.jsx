efc9c56314a535dfd6297fdec05e0fa9ed17e701
vivek-sahane <viveksahane1528@gmail.com>
2025-06-18 10:48:44 +0530

    Added new features and updates to the project

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -3,6 +3,8 @@ import { ShopContext } from "../Context/ShopContext";
 import { useParams } from "react-router-dom";
 import Bredcrums from "../Components/Bredcrums/BredCrums";
 import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+import Description from "../Components/Description/Description";
+import RelatedProducts from "../Components/RelatedProducts/RelatedProducts";
 
 const Product = () => {
   const { all_product } = useContext(ShopContext);
@@ -17,6 +19,8 @@ const Product = () => {
     <div>
       <Bredcrums product={product} />
       <ProductDisplay product={product} />
+      <Description/>
+      <RelatedProducts/>
     </div>
   );
 };
